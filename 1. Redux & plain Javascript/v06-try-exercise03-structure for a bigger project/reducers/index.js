439a63b90c0c79d83046999dc5684956663052c4
leo424y <leo424y@gmail.com>
2016-11-21 16:54:32 +0800

    just forget setting default

--- 1. Redux & plain Javascript/v06-try-exercise03-structure for a bigger project/reducers/index.js
+++ 1. Redux & plain Javascript/v06-try-exercise03-structure for a bigger project/reducers/index.js
@@ -1,38 +1,6 @@
 // step 2.2
-function counter(currentState, action){
-    var DEFAULT_STATE = 0;
-    if (currentState === undefined) { // look at to Note 1.1
 
-        nextState = DEFAULT_STATE;// Note1.2
-        return nextState;
-    }
-    switch (action.type) {
-        case 'DECREMENT': // look at Note2.1
-          nextState = currentState - 1;
-          return nextState;// Note2.2
-        case 'INCREMENT': // look at Note2.1
-          nextState = currentState + 1;
-          return nextState;// Note2.2
-        default:
-          nextState = currentState;
-        }
-}
-function sum(currentState, action){
-    var DEFAULT_STATE = 3;
-    if (currentState === undefined) { // look at to Note 1.1
 
-        nextState = DEFAULT_STATE;// Note1.2
-        return nextState;
-    }
-    switch (action.type) {
-        case 'SUM':
-          nextState = parseInt(action.a) + parseInt(action.b);
-          return nextState;
-        default:
-          nextState = currentState;
-        }
-    return nextState;
-}
 function combineReducer(currentState, action) {
     var DEFAULT_STATE = {count:0, sum:3};
     var nextState = Object.assign({},currentState);
