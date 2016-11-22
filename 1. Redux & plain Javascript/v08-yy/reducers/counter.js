ba31a3edc442ea6ab5ba2bb2dc4ca652c679b982
leo424y <leo424y@gmail.com>
2016-11-22 15:47:23 +0800

    ADD loading state

--- 1. Redux & plain Javascript/v08-yy/reducers/counter.js
+++ 1. Redux & plain Javascript/v08-yy/reducers/counter.js
@@ -1,5 +1,5 @@
 function counter(currentState, action){
-    var DEFAULT_STATE = 0;
+    var DEFAULT_STATE = {result:0, loading:false};
     if (currentState === undefined) { // look at to Note 1.1
 
         nextState = DEFAULT_STATE;// Note1.2
@@ -7,10 +7,11 @@ function counter(currentState, action){
     }
     switch (action.type) {
       case 'DECREMENT': // look at Note2.1
-        nextState = currentState - 1;
+        nextState.result = currentState - 1;
         return nextState;// Note2.2
       case 'INCREMENT': // look at Note2.1
-        nextState = currentState + 1;
+        nextState.result = currentState + 1;
+        nextState.loading = false;
         return nextState;// Note2.2
       default:
         nextState = currentState;
