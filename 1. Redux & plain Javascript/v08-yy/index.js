1eb36895b2844cfca3e6b06ba37483299f7974d1
leo424y <leo424y@gmail.com>
2016-11-22 09:00:58 +0800

    add logger as middleware function

--- 1. Redux & plain Javascript/v08-yy/index.js
+++ 1. Redux & plain Javascript/v08-yy/index.js
@@ -1,8 +1,17 @@
+// middleware
+const logger = function(store){
+  return function(next) {
+    return function(action) {
+      next(action);
+    }
+  }
+}
+
 // step 1.2
-var store = Redux.createStore(combineReducer)
+var store = Redux.createStore(combineReducer, Redux.applyMiddleware(logger))
 // ~end step 1.2
 // step 1.3
-      
+
 function render() {
     var state = store.getState();
     document.getElementById('value').innerHTML = state.count;
@@ -12,4 +21,3 @@ function render() {
 store.subscribe(render);
 // ~end step 1.3
 render();
-
