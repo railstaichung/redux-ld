e811ad6b2990e9f0b566a4b43555bbe936f0c60b
leo424y <leo424y@gmail.com>
2016-11-24 14:47:53 +0800

    es6 finished

--- 3. Redux & ES6/v01. yy/actions/index.js
+++ 3. Redux & ES6/v01. yy/actions/index.js
@@ -11,11 +11,11 @@ var getSum = (a, b) => ({ type: 'SUM', a:a, b:b });
 var asyncIncrease = (dispatch, state) => {
     dispatch({type:"INCREMENT_LOADING"});
     _fakeServerApi.increaseCount(state.count.result,
-        (data) => dispatch({ type: 'INCREMENT' }));
+        data => dispatch({ type: 'INCREMENT' }));
 }
 
 var getRandomImages = (dispatch, state) => {
   dispatch({ type: 'IMAGES_LOADING' });
   var imgurAPI = "https://api.imgur.com/3/gallery/random/random/1";
-  $.getJSON(imgurAPI).done((data) => dispatch({ type: 'IMAGES', data:data.data}))
+  $.getJSON(imgurAPI).done(data => dispatch({ type: 'IMAGES', data:data.data}))
 }
